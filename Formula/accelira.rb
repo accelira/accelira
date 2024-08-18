789c91b4449e10ccdfe76b1f04a1572585133b32
michael.li <michael.li@thoughtworks.com>
2024-08-18 11:57:40 +0800

    accelira 1.0.0 (new formula).

---
+++ Formula/accelira.rb
@@ -0,0 +1,17 @@
+
+
+  class Accelira < Formula
+    desc "Accelira performance testing tool"
+    homepage "https://github.com/accelira/accelira"
+    url "https://github.com/accelira/accelira/releases/download/v1.0.0/accelira-v1.0.0.tar.gz"
+    sha256 "eebd2e62eab691ad74cd4ab7ea2fa3029f3c4091ea3c5ed02a4a1778534ccdb4"
+    license "MIT"
+  
+    def install
+      bin.install "accelira"
+    end
+  
+    test do
+      assert_match "Accelira performance testing tool", shell_output("#{bin}/accelira --help")
+    end
+  end
