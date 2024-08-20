80216fda7c68ccda5d91d5c78bd498790527d529
michael.li <michael.li@thoughtworks.com>
2024-08-21 06:36:39 +0800

    make http request blzing fast

--- httpclient/httpclient.go
+++ httpclient/httpclient.go
@@ -20,18 +20,100 @@ type HttpResponse struct {
 	DNSLookupLatency    time.Duration
 }
 
-func HttpRequest(url, method string, body io.Reader, metricsChannel chan<- metrics.Metrics) (HttpResponse, error) {
-	start := time.Now()
+// func HttpRequest(url, method string, body io.Reader, metricsChannel chan<- metrics.Metrics) (HttpResponse, error) {
 
-	var dnsStart, dnsEnd, connectStart, connectEnd time.Time
+// 	var dnsStart, dnsEnd, connectStart, connectEnd, WroteRequestTime time.Time
+
+// 	trace := &httptrace.ClientTrace{
+// 		DNSStart:     func(info httptrace.DNSStartInfo) { dnsStart = time.Now() },
+// 		DNSDone:      func(info httptrace.DNSDoneInfo) { dnsEnd = time.Now() },
+// 		ConnectStart: func(network, addr string) { connectStart = time.Now() },
+// 		ConnectDone:  func(network, addr string, err error) { connectEnd = time.Now() },
+// 		// GotConn: func(info httptrace.GotConnInfo) {
+// 		// 	ConnGetting = time.Now()
+// 		// },
+// 		GotFirstResponseByte: func() {
+// 			fmt.Printf("Got first response byte: %v\n", time.Now().Sub(WroteRequestTime))
+// 		},
+// 		WroteRequest: func(info httptrace.WroteRequestInfo) {
+// 			WroteRequestTime = time.Now()
+
+// 		},
+// 	}
+
+// 	req, err := http.NewRequest(method, url, body)
+// 	if err != nil {
+// 		return HttpResponse{}, err
+// 	}
+
+// 	req = req.WithContext(httptrace.WithClientTrace(req.Context(), trace))
+// 	req.Header.Set("User-Agent", "Accelira perf testing tool/1.0")
+
+// 	transport := &http.Transport{
+// 		DisableKeepAlives: false,
+// 	}
+
+// 	client := &http.Client{
+// 		Transport: transport,
+// 	}
+
+// 	start := time.Now()
+// 	resp, err := client.Do(req)
+// 	if err != nil {
+// 		return HttpResponse{}, err
+// 	}
+// 	duration := time.Since(start)
+// 	defer resp.Body.Close()
+
+// 	responseBody, err := io.ReadAll(resp.Body)
+// 	if err != nil {
+// 		return HttpResponse{}, err
+// 	}
+
+// 	tcpHandshakeLatency := connectEnd.Sub(connectStart)
+// 	dnsLookupLatency := dnsEnd.Sub(dnsStart)
+
+// 	metrics := collectMetricsWithLatencies(url, method, len(responseBody), len(req.URL.String()), resp.StatusCode, duration, tcpHandshakeLatency, dnsLookupLatency)
+// 	sendMetrics(metrics, metricsChannel)
+
+// 	return HttpResponse{
+// 		Body:                string(responseBody),
+// 		StatusCode:          resp.StatusCode,
+// 		URL:                 url,
+// 		Method:              method,
+// 		Duration:            duration,
+// 		TCPHandshakeLatency: tcpHandshakeLatency,
+// 		DNSLookupLatency:    dnsLookupLatency,
+// 	}, nil
+// }
+
+var (
+	sharedTransport = &http.Transport{
+		DisableKeepAlives: false,
+	}
+	sharedClient = &http.Client{
+		Transport: sharedTransport,
+	}
+)
+
+func HttpRequest(url, method string, body io.Reader, metricsChannel chan<- metrics.Metrics) (HttpResponse, error) {
+
+	var dnsStart, dnsEnd, connectStart, connectEnd, wroteRequestTime, GotFirstResponseByteTime time.Time
 
 	trace := &httptrace.ClientTrace{
 		DNSStart:     func(info httptrace.DNSStartInfo) { dnsStart = time.Now() },
 		DNSDone:      func(info httptrace.DNSDoneInfo) { dnsEnd = time.Now() },
 		ConnectStart: func(network, addr string) { connectStart = time.Now() },
 		ConnectDone:  func(network, addr string, err error) { connectEnd = time.Now() },
+		GotFirstResponseByte: func() {
+			GotFirstResponseByteTime = time.Now()
+		},
+		WroteRequest: func(info httptrace.WroteRequestInfo) {
+			wroteRequestTime = time.Now()
+		},
 	}
 
+	// start := time.Now()
 	req, err := http.NewRequest(method, url, body)
 	if err != nil {
 		return HttpResponse{}, err
@@ -40,26 +122,19 @@ func HttpRequest(url, method string, body io.Reader, metricsChannel chan<- metri
 	req = req.WithContext(httptrace.WithClientTrace(req.Context(), trace))
 	req.Header.Set("User-Agent", "Accelira perf testing tool/1.0")
 
-	transport := &http.Transport{
-		DisableKeepAlives: false,
-	}
-
-	client := &http.Client{
-		Transport: transport,
-	}
-
-	resp, err := client.Do(req)
+	resp, err := sharedClient.Do(req)
 	if err != nil {
 		return HttpResponse{}, err
 	}
 	defer resp.Body.Close()
 
+	// duration := time.Since(start)
+	duration := GotFirstResponseByteTime.Sub(wroteRequestTime)
 	responseBody, err := io.ReadAll(resp.Body)
 	if err != nil {
 		return HttpResponse{}, err
 	}
 
-	duration := time.Since(start)
 	tcpHandshakeLatency := connectEnd.Sub(connectStart)
 	dnsLookupLatency := dnsEnd.Sub(dnsStart)
 
